0c640504043e9011cffb1cdffe0a9fe40c3609e7
HusseinAhmed90 <hussein.saeed90@outlook.com>
2021-04-14 10:59:22 +0200

    Using viewModel as variable in data binding

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -49,28 +49,24 @@ class GameFragment : Fragment() {
                 false
         )
 
-        // Get the viewmodel
+        // Get the viewMgaodel
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        // TODO (03) Pass the GameViewModel into the data binding - then you can remove the
+        //  (03) Pass the GameViewModel into the data binding - then you can remove the
         // OnClickListener setup from here
-        binding.correctButton.setOnClickListener {
-            viewModel.onCorrect()
-        }
-        binding.skipButton.setOnClickListener {
-            viewModel.onSkip()
-        }
+
+        binding.viewModel = viewModel
 
         /** Setting up LiveData observation relationship **/
-        viewModel.word.observe(this, Observer { newWord ->
+        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
             binding.wordText.text = newWord
         })
 
-        viewModel.score.observe(this, Observer { newScore ->
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
         })
 
-        viewModel.currentTime.observe(this, Observer { newTime ->
+        viewModel.currentTime.observe(viewLifecycleOwner, Observer { newTime ->
             binding.timerText.text = DateUtils.formatElapsedTime(newTime)
 
         })
